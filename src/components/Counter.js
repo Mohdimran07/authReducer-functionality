68a145d779326f6e60f0e2eec6ac0f678f1d0817
Mohammed Imran <mohdimran97057@gmail.com>
2021-12-03 20:28:24 +0530

    first commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,13 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const disptach = useDispatch();
   const counter = useSelector(state => state.counter);
+  const incrementby2Handler = () => {
+    disptach({ type: 'incrementby2'})
+  }
 
+  const decrementby2Handler = () => {
+    disptach({ type: 'decrementby2'})
+  }
   const incrementHandler = () => {
     disptach({ type: 'increment'})
   };
@@ -20,8 +26,10 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button onClick={incrementHandler}>IncrementBy2</button>
-        <button onClick={decrementHandler}>DecrementBy2</button>
+        <button onClick={incrementby2Handler}>IncrementBy2</button>
+        <button onClick={decrementby2Handler}>DecrementBy2</button><br></br>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
