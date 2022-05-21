af1f94a8945489da8a837140e3133457d94afdc3
Mohammed Imran <mohdimran97057@gmail.com>
2022-05-21 20:11:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Auth from './components/Auth';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
   return (
+    <>
+    <Header />
+    <UserProfile />
     <Counter />
+    <Auth />
+    </>
   );
 }
 
